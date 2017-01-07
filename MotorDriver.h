5e351a954bd27613668f9fef61b30cea0922c27e
Siddharth Deore <siddharthdeore@gmail.com>
2017-01-07 13:06:50 +0530

    Update MotorDriver.h

--- MotorDriver.h
+++ MotorDriver.h
@@ -2,21 +2,23 @@
 #define MotorDriver_h
 
 #if ARDUINO >= 100
-  #include "Arduino.h"
+#include "Arduino.h"
 #else
-  #include "WProgram.h"
-  #include "pins_arduino.h"
-  #include "WConstants.h"
+#include "WProgram.h"
+#include "pins_arduino.h"
+#include "WConstants.h"
 #endif
 class Motor
 {
   public:
     Motor(int pin_pwm, int pin_A, int pin_B);
+    Motor(HardwareSerial *serial);
     void rotate(int speed);
     void stop();
   private:
-	int limiter(int val,int upper, int lower);
-    int _pin_pwm, _pin_A, _pin_B,_speed;	
+    int limiter(int val, int upper, int lower);
+    int _pin_pwm, _pin_A, _pin_B, _speed;
+    HardwareSerial * _HardSerial;
 };
 
 #endif
